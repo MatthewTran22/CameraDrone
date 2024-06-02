0a5df82d010fa0fbf79cabc3801797116fffd476
Matthew Tran <matthewtran0122@gmail.com>
2024-06-01 21:41:13 -0700

    More playing around with API commands

--- DroneCommands/testLaunch.py
+++ DroneCommands/testLaunch.py
@@ -1,11 +1,21 @@
 from djitellopy import tello
+import cv2
 
 camdroid = tello.Tello()
 
 camdroid.connect()
-camdroid.takeoff()
-camdroid.move_left(20)
-camdroid.move_right(30)
+camdroid.streamoff()
+camdroid.streamon()
+
+while True:
+    frame = camdroid.get_frame_read()
+    currFrame = frame.frame
+    img = cv2.resize(currFrame, (600,600))
+    cv2.imshow("Drone View", img)
+    if cv2.waitKey(1) == ord('q'):
+        break
 print(camdroid.get_battery())
 
 camdroid.land()
+camdroid.streamoff()
+cv2.destroyAllWindows()

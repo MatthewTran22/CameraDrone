bf32518185f42bcb842e7d6d715e7bdd7c929f0c
Matthew Tran <matthewtran0122@gmail.com>
2024-06-01 22:41:37 -0700

    Update testLaunch.py

--- DroneCommands/testLaunch.py
+++ DroneCommands/testLaunch.py
@@ -11,6 +11,7 @@ while True:
     frame = camdroid.get_frame_read()
     currFrame = frame.frame
     img = cv2.resize(currFrame, (600,600))
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Changes the blue coloring back to yellow AKA code is racist
     cv2.imshow("Drone View", img)
     if cv2.waitKey(1) == ord('q'):
         break

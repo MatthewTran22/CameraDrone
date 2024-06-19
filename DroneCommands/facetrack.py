8779384fd7161c0de8b0c81c68eea740ee46b51e
Matthew Tran <matthewtran0122@gmail.com>
2024-06-19 16:24:12 -0700

    update
    Not going to use control panel, delay is too large

--- DroneCommands/facetrack.py
+++ DroneCommands/facetrack.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
 
+def runCam(cam):
+    while True:
+        ret, frame = cam.read()
+        cv2.imshow('webcam', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
 cap = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cap.read()
-    cv2.imshow('webcam', frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+runCam(cap)

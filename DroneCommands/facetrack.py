f6e67b56de9f1573cf4feff121bfae00c55fe1c0
Matthew Tran <matthewtran0122@gmail.com>
2024-06-26 16:34:08 -0700

    facetrack start

--- DroneCommands/facetrack.py
+++ DroneCommands/facetrack.py
@@ -1,12 +1,21 @@
 import cv2
 
 def findFace(img):
-    faceCascade = cv2.CascadeClassifier("Resources/harrcascade_frontalface_default.xml")
+    faceCascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(imgGray,1.2,8)
+
+    myFaceListC = []
+    myFaceListArea = []
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h), (0,0,255), 2)
 
 cap = cv2.VideoCapture(0)
 while True:
     _, img = cap.read()
+    findFace(img)
     cv2.imshow("Test", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        break

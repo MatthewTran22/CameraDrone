d8289f50db359f94add41cec0aa5d7b4b5a4cecd
Matthew Tran <matthewtran0122@gmail.com>
2024-06-26 16:46:41 -0700

    Update facetrack.py

--- DroneCommands/facetrack.py
+++ DroneCommands/facetrack.py
@@ -10,11 +10,23 @@ def findFace(img):
 
     for(x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h), (0,0,255), 2)
+        cx = x + w // 2
+        cy = y + h // 2
+        area = w * h
+        cv2.circle(img, (cx,cy),5,(0,255,0), cv2.FILLED)
+        myFaceListC.append([cx, cy])
+        myFaceListArea.append(area)
+    if len(myFaceListArea) != 0:
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i], myFaceListArea[i]]
+    else:
+        return img, [[0,0],0]
 
 cap = cv2.VideoCapture(0)
 while True:
     _, img = cap.read()
-    findFace(img)
+    img, info = findFace(img)
+    print("AREA: ", info[1], " CENTER: ", info[0])
     cv2.imshow("Test", img)
     if cv2.waitKey(1) == ord('q'):
         cv2.destroyAllWindows()

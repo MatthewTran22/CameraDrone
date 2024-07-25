dee027c16d0f7c8482416ee31f7ecacca098b537
Matthew Tran <matthewtran0122@gmail.com>
2024-07-25 14:34:58 -0700

    Update facetrack.py

--- DroneCommands/facetrack.py
+++ DroneCommands/facetrack.py
@@ -1,4 +1,21 @@
 import cv2
+import numpy as np
+from djitellopy import tello
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+me.streamon()
+me.takeoff()
+me.send_rc_control(0,0,25,0)
+time.sleep(2.2)
+
+
+w,h = 360, 240
+fbRange = [6200, 6800]
+pid = [0.4, 0.4, 0]
+pError = 0
+
 
 def findFace(img):
     faceCascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
@@ -21,13 +38,35 @@ def findFace(img):
         return img, [myFaceListC[i], myFaceListArea[i]]
     else:
         return img, [[0,0],0]
+    
+def trackFace(me, info, w, pid, pError):
+    area = info[1]
+    x,y = info[0]
+    fb = 0
 
-cap = cv2.VideoCapture(0)
+    error = x - w//2
+    speed = pid[0]*error + pid[1] * (error - pError)
+    speed = int(np.clip(speed, -100,100))
+    
+    if area > fbRange[0] and area < fbRange[1]:
+        fb = 0
+    elif area > fbRange[1]:
+        fb = -20
+    elif area < fbRange[0]:
+        fb = 20
+    if x == 0:
+        speed = 0
+        error = 0
+
+    me.send_rc_control(0,fb,0,speed)
+    return error
+
 while True:
-    _, img = cap.read()
+    img = me.get_frame_read().frame
+    img = cv2.resize(img,(w,h))
     img, info = findFace(img)
-    print("AREA: ", info[1], " CENTER: ", info[0])
+    pError = trackFace(me, info, w, pid, pError)
     cv2.imshow("Test", img)
     if cv2.waitKey(1) == ord('q'):
-        cv2.destroyAllWindows()
+        me.land()
         break
